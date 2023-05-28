1595c1074bf2465c43f6e2d23d91e0325fe9be6e
nevermind322 <alexfilonov2015@yandex.ru>
2023-05-28 19:25:31 +0300

    implement battle screen

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'home#index'
+  get "/battle", to: "battle#battle"
 
   # auth
   get "/login", to: "auth#new"
