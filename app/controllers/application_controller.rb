ded03b36332ad3b03620b95f6f8198855e6e5804
Ni Tail <wrumwrum@list.ru>
2023-05-28 19:09:41 +0300

    Add models and controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_locale
+
+  protected
+  def authorize_user
+    redirect_to login_path unless current_user
+  end
+
+  def set_locale
+    I18n.locale = session[:locale] || I18n.default_locale
+    session[:locale] = I18n.locale
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:current_user])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'home#index'
+
+  # auth
+  get "/login", to: "auth#new"
+  post "/login", to: "auth#create"
+  get "/signup", to: "auth#signup"
+  post "/signup", to: "users#create"
+  get "/logout", to: "auth#destroy"
+
+
+
+  get "/info", to: "info#index"
+
+
 end
